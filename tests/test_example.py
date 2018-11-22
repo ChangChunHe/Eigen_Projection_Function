2eb924808d279400036f038da2a76447e62fd0e6
ChangChunHe <731176792@qq.com>
2018-11-22 20:29:43 +0800

    add test example and support files

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 19 20:39:14 2018
 
-@author: hecc
-"""
 import unittest
 import numpy as np
 import sys
@@ -12,7 +8,10 @@ sys.path.append('..')
 sys.path.append('../IO')
 from vasp import read_vasp
 from EPF import Structure, StructureDifference
+
+
 class TestEPF(unittest.TestCase):
+    
     
     def setUp(self):
         self.lattice = np.array([[1,0,0],[0,1,0],[0,0,1]])
@@ -20,13 +19,16 @@ class TestEPF(unittest.TestCase):
         self.positions1 = np.array([[0.5,0.5,0.5], [0,1,0],[1,0,0],[1,1,1],[0,0,1]])
         self.positions2 = np.array([[0.7,0.7,0.7], [0,1,0],[1,0,0],[1,1,1],[0,0,1]])
     
+    
     def test_cart(self):
         S = read_vasp('primitive_cell_cart.vasp')
         S.draw_EPA()
         
+        
     def test_direct(self):
         S = read_vasp('primitive_cell_direct.vasp')
         S.draw_EPA()
+        
         
     def test_CH4(self):
         positions = self.positions1 * 1.09 *2/np.sqrt(3)
@@ -34,15 +36,18 @@ class TestEPF(unittest.TestCase):
         equal_atoms = np.unique(S.get_equivalent_atoms())
         self.assertEqual(len(equal_atoms),2)
     
+    
     def test_draw_EPF(self):
         from vasp import read_vasp
         S = read_vasp('primitive_cell_cart.vasp')
         S.draw_EPA()
         
+        
     def test_CHH3(self):
         positions = self.positions2 * 1.09 * 2/np.sqrt(3)
         S = Structure(self.lattice,positions,self.atoms,iscluster=True)
         equal_atoms = np.unique(S.get_equivalent_atoms())
+        S.draw_EPA()
         self.assertEqual(len(equal_atoms),3)
         
         
@@ -52,7 +57,7 @@ class TestEPF(unittest.TestCase):
         positions = self.positions2 * 1.09 * 2/np.sqrt(3)
         S2 = Structure(self.lattice,positions,self.atoms,iscluster=True)
         SD = StructureDifference(S1,S2)
-        diff,corresp = SD.get_structure_difference()
+        diff,corresp = SD.get_structure_mindisance_diff()
         self.assertAlmostEqual(diff, 0.7607215670542145)
 
 
@@ -60,8 +65,23 @@ class TestEPF(unittest.TestCase):
         S = read_vasp('primitive_cell_cart.vasp')
         equal_atoms = np.unique(S.get_equivalent_atoms())
         self.assertEqual(len(equal_atoms),1)
+
+
+    def test_decrease_difference_cluster(self):
+        S1,S2 = read_vasp('POSCAR1',iscluster=True),read_vasp('POSCAR2',iscluster=True)
+        SD = StructureDifference(S1,S2)
+        A, B = SD.get_structure_decdistance_diff(2)
+        self.assertEqual(np.shape(B)[0],60)
         
-        
+    
+    def test_decrease_difference_periodic(self):
+        S1 = read_vasp('POSCAR-1')
+        S2 = read_vasp('POSCAR-2')
+        SD = StructureDifference(S1,S2)
+        A,B = SD.get_structure_decdistance_diff(Rcut=2)
+        self.assertEqual(np.shape(B)[0],12)
+
+
 if __name__ == "__main__":
     import nose2
     nose2.main()
